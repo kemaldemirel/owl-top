d918d1cb5f290230673b5438d8fe62e108581b2b
kemal <demirelkd0696@gmail.com>
2022-04-17 00:24:12 +0300

    add static generate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { WhithLayout } from '../components/Layout/Layout';
 import { Htag, Button, Ptag, Tag, Rating } from '../UI';
+import axios from 'axios';
+import { MenuItem } from '../types/menu.interfaces';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(3);
 
   return (
@@ -50,8 +53,35 @@ function Home(): JSX.Element {
       </Tag>
 
       <Rating rating={rating} isEditable setRating={setRating} />
+
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default WhithLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
